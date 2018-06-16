4bb42d3912db0f605187067a7396aa0ee138b740
q_kul <q_kul@mail.ru>
2018-06-16 13:29:23 +0300

    my project

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies; // list movies
+        public SelectList genres; // В этом списке пользователь может выбрать жанр фильма.
+        public string movieGenre { get; set; }// соержит выбранный жанр
+    }
+}
