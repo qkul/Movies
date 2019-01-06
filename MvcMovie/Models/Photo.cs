47fb13c6e4dba31abaa59a74b314b5573291f608
q_kul <q_kul@mail.ru>
2019-01-06 21:02:38 +0300

    first changes

---
+++ MvcMovie/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public byte[] Image { get; set; }
+        public string ImageMimeType { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
